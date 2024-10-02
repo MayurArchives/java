3137748e36afb7457458710553606d2a4ac4a8fe
MayurArchives <mayurkanyal10@gmail.com>
2024-10-02 11:15:10 +0530

    Initial commit

---
+++ loop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int i = 2;
+        while (i <= 5) {
+            System.out.println("hello world!!");
+
+            i++;
+        }
+    }
+}
