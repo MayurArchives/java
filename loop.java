9974a9b8d783041d5ffa78bece51bd102c94b93b
MayurArchives <mayurkanyal10@gmail.com>
2024-10-02 12:35:19 +0530

    for loop

--- loop.java
+++ loop.java
@@ -2,11 +2,14 @@ import java.util.*;
 
 class HelloWorld {
     public static void main(String[] args) {
-        int i = 2;
-        while (i <= 5) {
-            System.out.println("hello world!!");
+        // int i = 2;
+        // while (i <= 5) {
+        // System.out.println("hello world!!");
 
-            i++;
+        // i++;
+        // }
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("hello world");
         }
     }
 }
