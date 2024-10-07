a8e8faf71c06968775806f7f6b175b3dd89bc6e4
MayurArchives <mayurkanyal10@gmail.com>
2024-10-07 22:28:39 +0530

    pattern print reverse

---
+++ pattern2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class pattern2 {
+
+    public static void main(String[] arg) {
+
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
