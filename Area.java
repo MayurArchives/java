c4f4e212a6e89d0f3941e6f2105625dc480d5060
MayurArchives <mayurkanyal10@gmail.com>
2024-10-07 21:52:30 +0530

    pattern print

---
+++ Area.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double radius = sc.nextDouble();
+        double area = (22 * radius * radius) / 7;
+
+        System.out.println(area);
+
+    }
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
         // int i = 2;
         // while (i <= 5) {
