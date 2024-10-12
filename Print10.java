70d08e770464a588be69d8403316c9124359bdfb
MayurArchives <mayurkanyal10@gmail.com>
2024-10-13 02:46:44 +0530

    HW_Print the day

---
+++ Print10.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Print10 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        switch (N) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thusday");
+                break;
+
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("Invalid input! Please enter a number between 1 and 7.");
+                break;
+
+        }
+
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
