6ea6bb3d7dbc4b1dc51a585d4437ac72580c4a3e
MayurArchives <mayurkanyal10@gmail.com>
2024-10-13 13:10:45 +0530

    Male or Female

---
+++ Print11.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Print11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+
+        if (ch == 'M' || ch == 'm') {
+            System.out.println("You are a male");
+        } else if (ch == 'F' || ch == 'f') {
+            System.out.println("You are a female");
+        } else {
+            System.out.println("Type again");
+        }
+    }
+}
