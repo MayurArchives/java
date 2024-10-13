0c3cdd98ef2861bf74cbc9db5132dd96374ad476
MayurArchives <mayurkanyal10@gmail.com>
2024-10-13 13:42:52 +0530

    HW_Marks and Rank

---
+++ Print12.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Print12 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int r = sc.nextInt();
+
+        if (m < 20 || r > 100) {
+            System.out.println("Needs improvement");
+        } else if (m < 40 || r > 80) {
+            System.out.println("Concentrate");
+        } else if (m < 60 || r > 120) {
+            System.out.println("Needs to focus");
+        } else if (m > 100 || r < 10) {
+            System.out.println("Very good");
+        } else {
+            System.out.println("Bright Student");
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
