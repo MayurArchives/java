2456498bdd311f34151496e3e30e84e8b08fb3e2
MayurArchives <mayurkanyal10@gmail.com>
2024-10-16 01:18:06 +0530

    Print 2,9,16

---
+++ Print20.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Print20 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= n; i += 7) {
+            System.out.println(i);
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
