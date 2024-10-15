322bceec5f67291b28bd9d58fea0a67bb5b9f976
MayurArchives <mayurkanyal10@gmail.com>
2024-10-15 21:41:56 +0530

    Print n to x

---
+++ print17.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Print17 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        for (int i = n; i >= x; i--) {
+            System.out.println(i);
+        }
+
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
