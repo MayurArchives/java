6d8cc8d6ad1ae9767a96bb15576976353d43c281
MayurArchives <mayurkanyal10@gmail.com>
2024-10-15 22:01:34 +0530

    print odd from n to 1

---
+++ Print18.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; 7 * i <= n; i++) {
+            System.out.print(7 * i + " ");
+        }
+
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
---
+++ Print19.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Print19 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = n; i >= 1; i--) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+            /*
+             * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+             * class should be named Solution.
+             */
+        }
+    }
+}
