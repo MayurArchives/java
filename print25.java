1fd82700ecf4fd3a6b8cae523a12dedc692aae52
MayurArchives <mayurkanyal10@gmail.com>
2024-10-16 23:47:04 +0530

    HW_Print Series 3, 11, 19...

---
+++ print25.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class print25 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 3; i <= n; i = i + 8) {
+
+            System.out.print(i + " ");
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
