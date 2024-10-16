b3add47f73c155ea13d2d6de9d6d05db74af62c9
MayurArchives <mayurkanyal10@gmail.com>
2024-10-16 21:37:46 +0530

    multiple of 9

---
+++ Print22.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Print22 {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i =1; i<=n; i++) {
+            int x= 9*i;
+            
+            System.out.print(x+" ");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
