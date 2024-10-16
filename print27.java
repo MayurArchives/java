d623f4bedc585755c39be029c4f86199a0f187c9
MayurArchives <mayurkanyal10@gmail.com>
2024-10-17 00:05:30 +0530

    HW_Print n to 5

---
+++ print27.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class print27 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = n; i >= 5; i--) {
+
+            System.out.print(i + " ");
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
