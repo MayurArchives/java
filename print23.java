57d0c6e76503d97b33175a66d08bcec50aef24c8
MayurArchives <mayurkanyal10@gmail.com>
2024-10-16 23:39:59 +0530

    HW_Print first n multiples of 11

---
+++ print23.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class print23 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int x = 11 * i;
+            System.out.print(x + " ");
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
