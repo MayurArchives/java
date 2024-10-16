07d13f9185295ec7db3aa0129aece390e1d5b450
MayurArchives <mayurkanyal10@gmail.com>
2024-10-16 23:50:32 +0530

    HW_Print series 13, 18, 23, 28...

---
+++ print26.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class print26 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 13; i <= n; i = i + 5) {
+
+            System.out.print(i + " ");
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
