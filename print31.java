0bd4b949b5b79b623bd85a8e4ac0f8afae49464f
MayurArchives <mayurkanyal10@gmail.com>
2024-10-17 21:44:53 +0530

    Print Alternate Elements of a String

--- print31.java
+++ print31.java
@@ -5,10 +5,11 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 65; i <= 90; i++) {
-            char x = (char) i;
-            System.out.print(x + " ");
+        String s = sc.nextLine();
+        for (int i = 0; i < s.length(); i += 2) {
+            System.out.print(s.charAt(i));
         }
+
         /*
          * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
          * class should be named Solution.
