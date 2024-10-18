1dbdff812e465331e743828c2de0d60608a4f3ee
MayurArchives <mayurkanyal10@gmail.com>
2024-10-19 01:10:01 +0530

    Print numbers 1-n in separate lines.

--- print35.java
+++ print35.java
@@ -1,13 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class print35 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        for (int i = str.length() - 1; i >= 0; i--) {
-            System.out.print(str.charAt(i));
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
         }
         /*
          * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
