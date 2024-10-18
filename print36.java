eadf04de360f6393dcd04ca331a4931d35671fe5
MayurArchives <mayurkanyal10@gmail.com>
2024-10-19 01:22:00 +0530

    Print numbers 20 to n

---
+++ print36.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class print36 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 20; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
