75c6b2d5288e629106539345060fc1c85d4c4d7f
MayurArchives <mayurkanyal10@gmail.com>
2024-10-18 20:50:50 +0530

    Print n/3

---
+++ print33.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class print33 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i > 1; i /= 3) {
+
+            System.out.print(i + " ");
+        }
+
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
