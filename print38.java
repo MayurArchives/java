9501af855534ee5c2f044211512982873da4406d
MayurArchives <mayurkanyal10@gmail.com>
2024-10-19 18:57:22 +0530

    Printing 5 to N(While Loop)

---
+++ print38.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 5;
+        while (i <= n) {
+            System.out.println(i);
+            i++;
+        }
+
+        // for (int i = 5; i <= n; i++) {
+        // System.out.println(i);
+        // }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
