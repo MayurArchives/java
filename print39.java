7baa49c6635c33970fe229e9e64edd02e9c7c83d
MayurArchives <mayurkanyal10@gmail.com>
2024-10-19 20:02:04 +0530

    Print till n for this Series 5,11,17,23,29..

---
+++ print39.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class print39 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 5;
+        while (i <= n) {
+            System.out.println(i);
+            i += 6;
+        }
+
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
