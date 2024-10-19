e1fc3f86628303f2f07f761be33e983c212828f4
MayurArchives <mayurkanyal10@gmail.com>
2024-10-19 20:15:59 +0530

    Print In Range(x and y)

---
+++ print40.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class print40 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int i = x;
+        while (i <= y) {
+            System.out.println(i);
+            i++;
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
