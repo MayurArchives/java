58dddccd190ce5ea8f5340dd43559ed59018fda3
MayurArchives <mayurkanyal10@gmail.com>
2024-10-19 20:34:09 +0530

    Print from n to 3 using while loop

---
+++ print41.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class print41 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while (n <= 2) {
+            System.out.println(n);
+            n++;
+        }
+
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
