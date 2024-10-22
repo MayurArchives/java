6a5c45837bd3ea601c2faf9fa9f98eacb80933c6
MayurArchives <mayurkanyal10@gmail.com>
2024-10-22 19:52:23 +0530

    print z-a

---
+++ print51.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class print51 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 122; i >= 97; i--) {
+
+            char x = (char) i;
+
+            System.out.print(x + " ");
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
