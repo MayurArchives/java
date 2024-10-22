b132504539bd4482fb623a1480f0d282179e3c62
MayurArchives <mayurkanyal10@gmail.com>
2024-10-22 19:57:45 +0530

    Print all digits from end

--- print53.java
+++ print53.java
@@ -1,3 +1,21 @@
+import java.io.*;
+import java.util.*;
+
 public class print53 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n > 0) {
+            int num = (n % 10);
+            System.out.println(num);
+            n /= 10;
+
+        }
+
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
