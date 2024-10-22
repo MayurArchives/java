855865ed86d0bad0155cc175a3de84c3432df70b
MayurArchives <mayurkanyal10@gmail.com>
2024-10-23 00:13:25 +0530

    Print z, w, t...

---
+++ print59.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class print59 {
+
+    public static void main(String[] args) {
+
+        char ch = 'z';
+        while (ch >= 'd') {
+            System.out.print(ch + " ");
+            ch -= 3;
+        }
+
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
