bf9c131023b695f9e73d1a2d1b2db01c8c47e401
MayurArchives <mayurkanyal10@gmail.com>
2024-10-22 19:53:28 +0530

    Print n, n-3k, n-6k, n-12k,... till m.

--- print55.java
+++ print55.java
@@ -1,3 +1,20 @@
-public class print55 {
+import java.io.*;
+import java.util.*;
 
+public class print52 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int m = sc.nextInt();
+        for (int i = n; i >= m; i = i - 3 * k) {
+
+            System.out.print(i + " ");
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
 }
