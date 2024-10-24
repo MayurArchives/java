1861c18aa211b93e2e3cda1484b88f01f0ac9aab
MayurArchives <mayurkanyal10@gmail.com>
2024-10-24 21:38:22 +0530

    Pattern_12_Diamond

---
+++ print64.java
@@ -0,0 +1,42 @@
+public
+
+import java.io.*;
+import java.util.*;
+
+public class print64 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Read input
+
+        // Upper part of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+        }
+
+        // Lower part of the diamond
+        for (int i = 1; i <= n - 1; i++) {
+            // Print spaces
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= 2 * (n - i) - 1; k++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}{
+
+}
