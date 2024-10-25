47a21ac4cdefb8c21bd62fb68fe400d8dd3d76e7
MayurArchives <mayurkanyal10@gmail.com>
2024-10-25 21:17:33 +0530

    Calculate Area 3

---
+++ GreaterNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class GreaterNumber {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+
+        int greater = findGreater(num1, num2);
+        System.out.println("The greater number is: " + greater);
+    }
+
+    public static int findGreater(int a, int b) {
+        if (a > b) ? a : b;
+    }
+}
--- OddNum.java
+++ OddNum.java
@@ -2,6 +2,15 @@
 import java.util.Scanner;
 
 public class OddNum {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = OddNumbers(n);
+        System.out.println(sum);
+
+    }
+
     public static int OddNumbers(int n) {
         int sum = 0;
         for (int i = 1; i <= n; i++) {
@@ -10,14 +19,6 @@ public class OddNum {
             }
         }
         return sum;
-
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int sum = OddNumbers(n);
-        System.out.println(sum);
 
     }
 }
