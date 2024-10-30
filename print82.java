8c67fbca0e9d8151612213c9f0b78582632f4084
MayurArchives <mayurkanyal10@gmail.com>
2024-10-30 20:34:25 +0530

    Print Array element if index divisible by 3

--- print82.java
+++ print82.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class print79 {
+public class print82 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,7 +11,7 @@ public class print79 {
         for (int i = 0; i < n; i++) {
             array[i] = sc.nextInt();
         }
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = 0; i < n; i += 3) {
             System.out.print(array[i] + " ");
         }
         /*
