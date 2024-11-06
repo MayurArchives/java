7e0180a86021d7b9190251c0713cb9bd66807162
MayurArchives <mayurkanyal10@gmail.com>
2024-11-06 19:39:16 +0530

    pattern

--- print106.java
+++ print106.java
@@ -1,13 +1,19 @@
-
 import java.io.*;
 import java.util.*;
 
 public class print106 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        for (int i = 0; i < n / 2 + 1; i++) {
+            for (int j = 0; j < n / 2 - i; j++) {
+                System.out.print("\t");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
 
     }
-
 }
