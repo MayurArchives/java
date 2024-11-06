53b32819afb834401ff4baa50419d9fe85fb693e
MayurArchives <mayurkanyal10@gmail.com>
2024-11-06 23:32:45 +0530

    Print Pair

--- print106.java
+++ print106.java
@@ -5,6 +5,17 @@ public class print106 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
+        for (int i = 0; i < n / 2 + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("\t");
+            }
+            for (int k = 0; k < n - 2 * i; k++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+
         for (int i = 0; i < n / 2 + 1; i++) {
             for (int j = 0; j < n / 2 - i; j++) {
                 System.out.print("\t");
