6bb45348047edeb143c7ab87fc26750baaa6bc83
MayurArchives <mayurkanyal10@gmail.com>
2024-11-08 21:43:27 +0530

    Find Duplicate 3

--- print122.java
+++ print122.java
@@ -1,32 +1,31 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class print122 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         int[] arr = new int[n];
 
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        int left = sc.nextInt();
-        int right = sc.nextInt();
-        int x = sc.nextInt();
-        for (int i = 0; i <= left; i++) {
-            arr[i] += x;
-        }
-        for (int i = right; i < n; i++) {
-            arr[i] += x;
-        }
+
         for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("true");
+                    return;
+
+                }
+            }
         }
         /*
          * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
          * class should be named Solution.
          */
+
+        System.out.println("false");
     }
 }
