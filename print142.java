85657d3d8394a1b83256500b2283e01252b9f5f3
MayurArchives <mayurkanyal10@gmail.com>
2024-11-14 00:31:44 +0530

    odd number pairs

---
+++ print142.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class print142 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i] % 2 != arr[j] % 2) {
+                    System.out.println(arr[i] + " " + arr[j]);
+                }
+            }
+        }
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
