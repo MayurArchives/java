09907767dbcfeee8b7bfddbc52e3e8c510942747
MayurArchives <mayurkanyal10@gmail.com>
2024-11-23 12:00:20 +0530

    additive inverse find

---
+++ print153.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class print153 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr1[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        int m = sc.nextInt();
+        int arr2[] = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            int additiveInverse = -arr1[i];
+            boolean found = false;
+            for (int j = 0; j < m; j++) {
+                if (arr2[j] == additiveInverse) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                System.out.print(arr1[i] + " ");
+            }
+        }
+
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+    }
+}
